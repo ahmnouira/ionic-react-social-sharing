8628854b76460fb126aa084bf9b58519f6a61ae8
Ahmed Nouira <ahmnouira@gmail.com>
2021-07-07 23:37:02 +0100

    feat: add fake posts

---
+++ src/models/post.ts
@@ -0,0 +1,13 @@
+export type Post = {
+    id: number,
+    title: string,
+    date: string
+    category: string
+    source: string,
+    image: string
+    content: string
+    author: string
+}
+
+
+
