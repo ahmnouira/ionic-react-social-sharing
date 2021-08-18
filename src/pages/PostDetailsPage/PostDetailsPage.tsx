c3efef5075bbe7c74b53de41a58911338f05cefa
Ahmed Nouira <ahmnouira@gmail.com>
2021-08-18 23:23:40 +0100

    fix :fixes

--- src/pages/PostDetailsPage/PostDetailsPage.tsx
+++ src/pages/PostDetailsPage/PostDetailsPage.tsx
@@ -29,6 +29,35 @@ export function PostDetailsPage() {
   const params = useParams<ParamsType>()
   const [post, setPost] = useState<PostType | undefined>(undefined)
 
+
+  // this.dfdf == state
+
+  /** inputs **/
+  const [maximumHeight, setMaximumHeight] = React.useState<number>(350) // @input
+  const titleColor = '#AAA'
+  const expandedColor = '#313131'
+  const imageUrl = "https://picsum.photos/1080" || post?.image
+
+
+  const [header, setHeader] = React.useState<HTMLElement>()
+
+  const [toolbar, setToolbar] = React.useState<HTMLIonToolbarElement | null>()
+
+
+  const [toolbarBackground, setToolbarBackground] = React.useState<Element | null>()
+
+
+  const [ionTitle, setIonTitle] = React.useState<HTMLIonTitleElement | null>()
+  const [barButtons, setBarButton] = React.useState<HTMLIonButtonsElement | null>()
+  const [scrollContent, setScrollContent] = React.useState<Element | null>()
+
+  const [imageOverlay, setImageOverlay] = React.useState<HTMLElement>()
+  const [colorOverlay, setColorOverlay] = React.useState<HTMLElement>()
+
+
+  const [overlayTitle, setOverlayTitle] = React.useState<HTMLElement>()
+
+
   /** styles */
   const [headerStyles, setHeaderStyle] = React.useState<React.CSSProperties>()
   const [mainContentStyles, setMainContentStyles] = React.useState<React.CSSProperties>()
@@ -36,34 +65,58 @@ export function PostDetailsPage() {
   const [imageOverlayStyle, setImageOverlayStyle] = React.useState<React.CSSProperties>({})
   const [overlayTitleStyle, setOverlayTitleStyle] = React.useState<React.CSSProperties>({})
   const [toolbarTitleStyle, setToolbarTitleStyle] = React.useState<React.CSSProperties>({})
+  const [toolbarBackgroundStyle, setToolbarBackgroundStyle] = React.useState<React.CSSProperties>({})
+
 
   const [headerHeight, setHeaderHeight] = React.useState<number>(0)
+  const [headerMinHeight, setHeaderMinHeight] = React.useState<number>(0)
+
 
   const [translateAmt, setTranslateAmt] = React.useState<number>()
   const [scaleAmt, setScaleAmt] = React.useState<number>()
 
+  const [ticking, setTicking] = React.useState<boolean>(false)
+
+
+  const [scrollContentPaddingTop, setScrollContentPaddingTop] = React.useState<any>()
+
+  const [originalToolbarBgColor, setOriginalToolbarBgColor] = React.useState<string>()
+
+
+
   const contentRef = React.useRef(null)
   const headerRef = React.useRef<any>()
+
+
+  const [childern, setChildern]   = React.useState<JSX.Element[]>()
+
 
   // inits
   React.useEffect(() => {
     const header: HTMLIonHeaderElement = document.getElementsByTagName('ion-header')[0]
-
     const parentElement = header.parentElement
+
 
     if (!parentElement) throw new Error('No parentElemnt')
 
     const toolbar = header.querySelector('ion-toolbar')
+    setToolbar(toolbar)
 
     if (!toolbar) throw new Error('Parallax requires a <ion-toolbar> or navbar element on the page to work.')
-
+    
     const ionTitle = toolbar.querySelector('ion-title')
+
+    setIonTitle(ionTitle)
 
     if (!toolbar.shadowRoot) throw new Error('Parallax requires a <ion-toolbar> with shadow root.')
 
     const toolbarBackground = toolbar.shadowRoot.querySelector('.toolbar-background')
+    setToolbarBackground(toolbarBackground)
+
+    if (!toolbarBackground) throw new Error('No .toolbar-background')
 
     const barButtons = header.querySelector('ion-buttons')
+    setBarButton(barButtons)
 
     const ionContent = parentElement.querySelector('ion-content')
 
@@ -77,24 +130,116 @@ export function PostDetailsPage() {
       throw new Error('Parallax directive requires an <ion-content> element on the page to work.')
     }
 
-    console.log('main', scrollContent)
+    setScrollContent(scrollContent)
+
 
     /**  Copy title and buttons **/
     const overlayTitle = ionTitle && (ionTitle.cloneNode(true) as HTMLElement)
-
+    let childs: JSX.Element[] =[]
+    
+    
     if (overlayTitle) {
-      ////// this.renderer.addClass(this.overlayTitle, 'parallax-title');
-      setTimeout(() => {
-        const toolbarTitle = overlayTitle.shadowRoot?.querySelector('.toolbar-title')
+      // overlayTitle.classList.add('parallax-title')
+     
 
-        console.log('toolbar', toolbarTitle)
-
-        setToolbarTitleStyle({
+      const toolbarTitle = overlayTitle.shadowRoot?.querySelector('.toolbar-title')
+      setToolbarTitleStyle({
           pointerEvents: 'unset',
-        })
       })
     }
-  }, [])
+
+    if(barButtons) {
+      // childs.push(barButtons)
+    }
+
+    setChildern(childs)
+   
+    // init for styles
+
+    setHeaderHeight(scrollContent.clientHeight)
+    setTicking(false)
+
+    // fetch styles
+    setMaximumHeight(parseFloat(maximumHeight.toString()))
+
+    setHeaderMinHeight(toolbar.offsetHeight)
+
+    let scrollContentPaddingTop: number = parseFloat(
+      window.getComputedStyle(scrollContent, null).paddingTop.replace('px', '')
+    )
+
+    setScrollContentPaddingTop(scrollContentPaddingTop)
+
+
+    const originalToolbarBgColor = window.getComputedStyle(toolbarBackground, null).backgroundColor
+
+
+    if (!originalToolbarBgColor) { throw new Error('Error: toolbarBackround is null.'); }
+
+    setOriginalToolbarBgColor(originalToolbarBgColor)
+
+    // header and title
+
+    setHeaderStyle({
+      position: 'relative',
+    })
+
+    // rerender here
+    setOverlayTitleStyle({
+      color: titleColor,
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      textAlign: 'center',
+    })
+
+    // color overlay
+    setColorOverlayStyle({
+      backgroundColor: originalToolbarBgColor,
+      height: `${maximumHeight}px`,
+      position: 'absolute',
+      top: `${-headerMinHeight * 0}px`,
+      left: '0',
+      width: '100%',
+      zIndex: 10,
+      pointerEvents: 'none',
+    })
+
+    // image overlay
+
+    setImageOverlayStyle({
+      backgroundColor: expandedColor,
+      backgroundImage: `url(${imageUrl})`,
+      height: '100%',
+      width: '100%',
+      pointerEvents: 'none',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    })
+
+    setToolbarBackgroundStyle({
+        backgroundColor: originalToolbarBgColor
+
+    })
+
+    /*
+
+       // .bar-buttons
+       if (this.barButtons) {
+         this.renderer.setStyle(this.barButtons, 'pointer-events', 'all');
+         Array.from(this.barButtons.children).forEach(btn => {
+           this.renderer.setStyle(btn, 'color', this.titleColor);
+         });
+       }
+   
+       // .scroll-content
+       if (this.scrollContent) {
+         this.renderer.setAttribute(this.scrollContent, 'parallax', '');
+         this.renderer.setStyle(this.scrollContent, 'padding-top',
+           `${this.maximumHeight + this.scrollContentPaddingTop - this.headerMinHeight}px`);
+       }
+       */
+  }, [imageUrl])
 
   useEffect(() => {
     const header = headerRef.current
@@ -105,6 +250,14 @@ export function PostDetailsPage() {
 
   // handle scroll
   const handleOnIonScroll = (ev: any) => {
+
+
+
+    if(!scrollContent || !toolbar) {
+      return; 
+    }
+
+    const scrollTop = ev.detail.scrollTop; 
     if (ev.detail.scrollTop >= 0) {
       setTranslateAmt(ev.detail.scrollTop / 2)
       setScaleAmt(1)
@@ -113,16 +266,34 @@ export function PostDetailsPage() {
       setScaleAmt(-ev.detail.scrollTop / (headerHeight + 1))
     }
 
-    /*
         // Parallax total progress
-        this.headerMinHeight = this.toolbar.offsetHeight;
-        let progress = (this.maximumHeight - this.scrollTop - this.headerMinHeight) / (this.maximumHeight - this.headerMinHeight);
+        setHeaderMinHeight(toolbar.offsetHeight);
+        let progress = (maximumHeight - scrollTop - headerMinHeight) / (maximumHeight - headerMinHeight);
         progress = Math.max(progress, 0);
     
         // ion-header: set height
-        let targetHeight = this.maximumHeight - this.scrollTop;
-        targetHeight = Math.max(targetHeight, this.headerMinHeight);
-*/
+        let targetHeight = maximumHeight - scrollTop;
+        targetHeight = Math.max(targetHeight, headerMinHeight);
+
+       // .toolbar-background: change color
+       setImageOverlayStyle({
+        ...imageOverlayStyle,
+        height: `${targetHeight}px`,
+        opacity: `${progress}`,
+      })
+  
+      setColorOverlayStyle({
+        ...colorOverlayStyle,
+        height: `${targetHeight}px`,
+        opacity: targetHeight > headerMinHeight ? '1' : '0',
+      })
+
+      
+      setToolbarBackgroundStyle({
+        backgroundColor: targetHeight > headerMinHeight ? 'transparent': originalToolbarBgColor 
+      })
+
+      setTicking(false)
   }
 
   useEffect(() => {
@@ -139,26 +310,70 @@ export function PostDetailsPage() {
   return (
     <React.Fragment>
       <IonPage>
-        <IonHeader ref={headerRef}>
+        <IonHeader translucent mode='ios'>
+          <IonToolbar color="primary" style={{}}>
+            <IonButtons slot='start'>
+              <IonBackButton defaultHref="/" />
+            </IonButtons>
+            <IonTitle style={toolbarTitleStyle}>Post Details</IonTitle>
+            <IonButtons slot="end">
+              <IonButton>
+                Button
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+
+        
           <div className='color-overlay' style={colorOverlayStyle}>
             <div
               className='image-overlay'
-              style={{
-                ...imageOverlayStyle,
-              }}>
-              <IonToolbar>
-                <IonButtons slot='start'>
-                  <IonBackButton />
-                </IonButtons>
-                <IonTitle>Post Details</IonTitle>
-              </IonToolbar>
-            </div>
+              style={
+                imageOverlayStyle
+              }>          <IonToolbar>
+
+                 <IonButtons slot='start'>
+              <IonBackButton defaultHref="/" />
+            </IonButtons>
+            <IonTitle>Post Details</IonTitle>
+            <IonButtons slot="end">
+              <IonButton>
+                Button
+              </IonButton>
+            </IonButtons>
+            </IonToolbar>
+              </div>
           </div>
         </IonHeader>
-        <IonContent ref={contentRef} scrollEvents onIonScroll={handleOnIonScroll}>
+        <IonContent class="ion-padding" ref={contentRef} scrollEvents onIonScroll={handleOnIonScroll}>
+          
           <div style={{}}>
             <h2>{post?.title}</h2>
             <p>{post?.content}</p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
               industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
